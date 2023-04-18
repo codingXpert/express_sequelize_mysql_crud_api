3b0ffb59cbb1b15331aa5ee30bfb350d938405e4
Vivek <vivekraj78965.vr@gmail.com>
2023-04-18 15:51:10 +0530

    created product controller

---
+++ controllers/productController.js
@@ -0,0 +1,68 @@
+const db = require('../models');
+
+//create main Model
+const Product = db.products;
+const Review = db.reviews;
+
+//main work
+
+// 1. create product
+const addProduct = async(req ,res) => {
+
+    let info = {
+        title:req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        published: req.body.published ? req.body.published :false
+    }
+
+    const product = await Product.create(info);
+    res.status(200).send(product);
+}
+
+// 2. get all products
+const getAllProducts = async(req,res) => {
+    let products = await Products.findAll({
+        // attributes: [
+        //     'title',       //will return only the listed details, if sated attributes
+        //     'price',
+        // ]
+    });
+    res.status(200).send(products)
+}
+
+// 3. get single product
+const getOneProduct = async(req , res) => {
+    let id = req.params.id;
+    let product = await Product.findOne({where: {id: id}});
+    res.status(200).send(product)
+}
+
+// 4. update product
+const updateProduct = async(req , res) => {
+    let id = req.params.id;
+    let product = await Product.update(req.body , {where: {id: id}});
+    res.status(200).send(product)
+}
+
+// 3. delete product by id
+const deleteProduct = async(req , res) => {
+    let id = req.params.id;
+    await Product.destroy({where: {id: id}});
+    res.status(200).send('Product Is Deleted !')
+}
+
+// 4. get published product
+const getPublishedProduct = async(req , res) => {
+    let products = await Product.findAll( {where: {published: true}} );
+    res.status(200).send(products)
+}
+
+module.exports = {
+    addProduct,
+    getAllProducts,
+    getOneProduct,
+    updateProduct,
+    deleteProduct,
+    getPublishedProduct
+}
